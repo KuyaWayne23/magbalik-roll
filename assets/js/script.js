b1b622922e4079e651b75bae52398d2e1a8aa4f9
Orlie Wayne A. Faustorilla <orliewayne.faustorilla@gmail.com>
2021-02-17 19:51:54 +0800

    added more ricks to roll

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,6 @@ document.addEventListener('DOMContentLoaded', function () {
 		event.preventDefault();
 
 		// alert(`Video Has Played`);
-		document.getElementById('itchafrank').style.display = 'block';
+		document.getElementById('itchafrank').innerText = 'SIKE! YOU JUST GOT MAGBALIK-ROLLED!';
 	});
 });
